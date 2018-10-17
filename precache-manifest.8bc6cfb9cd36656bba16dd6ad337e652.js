d8d337220a8bba01807f1aa1b880172e7040cf73
Son Le <sontungle1998@gmail.com>
2018-10-16 23:20:23 -0400

    Updates

---
+++ precache-manifest.8bc6cfb9cd36656bba16dd6ad337e652.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a47a6ad649acaad9a3b39ae3ec7fbbf5",
+    "url": "/globomantics/static/media/GloboLogo.a47a6ad6.png"
+  },
+  {
+    "revision": "4fcd624cc978123b1f3be74dc0666017",
+    "url": "/globomantics/static/media/Email.4fcd624c.png"
+  },
+  {
+    "revision": "5cdc994888f314f730ce",
+    "url": "/globomantics/static/js/runtime~main.5cdc9948.js"
+  },
+  {
+    "revision": "5f33e80c7792920b554b",
+    "url": "/globomantics/static/js/main.5f33e80c.chunk.js"
+  },
+  {
+    "revision": "77a5950dbb2822c2d665",
+    "url": "/globomantics/static/js/1.77a5950d.chunk.js"
+  },
+  {
+    "revision": "5f33e80c7792920b554b",
+    "url": "/globomantics/static/css/main.2fe9531d.chunk.css"
+  },
+  {
+    "revision": "77a5950dbb2822c2d665",
+    "url": "/globomantics/static/css/1.297ffed1.chunk.css"
+  },
+  {
+    "revision": "1cd7c9f04b19a4b981f40b6b16cc5c88",
+    "url": "/globomantics/index.html"
+  }
+];
